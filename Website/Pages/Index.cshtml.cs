5e40653f2ddc8be29fc631550b664a9c9a1203d6
Richard Bach <richardbach9@ksu.edu>
2020-11-08 03:22:04 -0600

    Fixed website project error

---
+++ Website/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Sides;
+using BleakwindBuffet.Data.Drinks;
+namespace Website.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
